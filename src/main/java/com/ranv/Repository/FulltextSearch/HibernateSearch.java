83b4116e895f8e50747310e2c466d4f0259d2ba7
Andrey-2310 <andreyredkovskiy@mail.ru>
2017-07-26 18:18:27 +0300

    fulltext search added

---
+++ src/main/java/com/ranv/Repository/FulltextSearch/HibernateSearch.java
@@ -0,0 +1,43 @@
+package com.ranv.Repository.FulltextSearch;
+
+import com.ranv.Model.ModelDB.Manual;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.query.dsl.QueryBuilder;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.Collections;
+import java.util.List;
+
+
+@Repository
+@Transactional
+public class HibernateSearch {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Manual> fulltextSearching(String keyword){
+        if(keyword.equals("")) return Collections.emptyList();
+        FullTextEntityManager fullTextEntityManager =
+                org.hibernate.search.jpa.Search.getFullTextEntityManager(entityManager);
+        QueryBuilder qb = fullTextEntityManager.getSearchFactory()
+                .buildQueryBuilder().forEntity(Manual.class).get();
+        javax.persistence.Query jpaQuery =
+                fullTextEntityManager.createFullTextQuery(createQuery(qb, keyword), Manual.class);
+        @SuppressWarnings("unchecked")
+        List<Manual> result =  jpaQuery.getResultList();
+        return result;
+    }
+
+    private  org.apache.lucene.search.Query createQuery(QueryBuilder qb, String keyword){
+        return    qb
+                .keyword()
+                .onFields("name", "introduction", "user.username", "tags.name")
+                .matching(keyword)
+                .createQuery();
+    }
+
+
+}
