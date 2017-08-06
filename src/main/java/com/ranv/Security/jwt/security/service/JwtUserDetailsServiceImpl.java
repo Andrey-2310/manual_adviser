8b2e981283c443dcc84a912c75b93f2a39f77601
nareykov <nareykov@mail.ru>
2017-08-06 15:52:22 +0300

    security enabled

--- src/main/java/com/ranv/Security/jwt/security/service/JwtUserDetailsServiceImpl.java
+++ src/main/java/com/ranv/Security/jwt/security/service/JwtUserDetailsServiceImpl.java
@@ -1,17 +1,29 @@
 package com.ranv.Security.jwt.security.service;
 
-//@Service
-//@RequiredArgsConstructor
-//public class JwtUserDetailsServiceImpl implements UserDetailsService {
-//
-//    private final UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User byUsername = this.userRepository.findByUsername(username);
-//
-//        return Optional.ofNullable(byUsername)
-//                .map(JwtUserDetails::new)
-//                .orElseThrow(() -> new JsonException("User nor found."));
-//    }
-//}
+import com.ranv.Security.jwt.security.model.JwtUserDetails;
+import com.ranv.model.DB.User;
+import com.ranv.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.json.JsonException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User byUsername = this.userRepository.findByUsername(username);
+
+        return Optional.ofNullable(byUsername)
+                .map(JwtUserDetails::new)
+                .orElseThrow(() -> new JsonException("User nor found."));
+    }
+}
